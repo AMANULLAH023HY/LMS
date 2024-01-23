1c32636fe9cb835167e1bb023b76d5a1be6651ee
AMANULLAH023HY <amana170829@gmail.com>
2024-01-23 23:09:02 +0530

    complete file upload,using multer and cloudinary

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import User from "../model/user.model.js";
 import AppErorr from "../utils/error.util.js";
+import cloudinary from 'cloudinary';
+import fs from 'fs/promises';
+
 
 const cookieOption = {
     maxAge: 7*24*60*60*1000, // 7 days
@@ -38,6 +41,35 @@ const register = async (req,res, next)=>{
     }
 
     // TODO : file upload 
+
+
+    if(req.file){
+        try {
+            const result = await cloudinary.v2.uploader.upload(req.file.path,{
+                folder:"LSM",
+                width:250,
+                height:250,
+                gravity:'faces',
+                crop:'fill',
+            });
+
+            if(result){
+                user.avatar.public_url = result.public_id;
+                user.avatar.secure_url = result.secure_url;
+
+                // Remove file from server 
+                fs.rm(`uploads/${req.file.filename}`);
+
+            
+            }
+        } catch (error) {
+
+            return next(new AppErorr(error || 'File not uploaded, please try again', 500));
+            
+        }
+    }
+
+
 
     await user.save();
 
