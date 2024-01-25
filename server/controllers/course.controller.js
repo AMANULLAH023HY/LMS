53c8a952f4bfc0f36a72c4105249268661cf5033
AMANULLAH023HY <amana170829@gmail.com>
2024-01-25 23:09:36 +0530

    Course create delete update

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -1,52 +1,155 @@
-import Course from "../model/course.model.js"
+import { count } from "console";
+import Course from "../model/course.model.js";
 import AppErorr from "../utils/error.util.js";
+import cloudniry from "cloudinary";
+import fs from "fs/promises";
 
-const getAllCourses = async (req,res,next)=>{
+const getAllCourses = async (req, res, next) => {
+  const courses = await Course.find({}).select("-lectures");
 
-    const courses = await Course.find({}).select('-lectures'); 
+  try {
+    res.status(200).json({
+      success: true,
+      message: "All courses",
+      courses,
+    });
+  } catch (error) {
+    return next(new AppErorr(error.message, 500));
+  }
+};
 
-    try {
-        res.status(200).json({
-            success:true,
-            message:"All courses",
-            courses,
-    
-        })
-    } catch (error) {
-        return next(new AppErorr(error.message,500));
-        
+const getLectureByCourseId = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const course = await Course.findById(id);
+    if (!course) {
+      return next(new AppErorr("Invalid course id", 400));
     }
 
-   
+    res.status(200).json({
+      success: true,
+      message: "Course lectures fetched successfully!",
+      lectures: course.lectures,
+    });
+  } catch (error) {
+    return next(new AppErorr(error.message, 500));
+  }
+};
 
-}
+const createCourse = async (req, res, next) => {
+  const { title, description, category, createdBy } = req.body;
 
-const getLectureByCourseId = async (req,res,next)=>{
+  if (!title || !description || !category || createdBy) {
+    return next(new AppErorr("All fields are required ", 400));
+  }
 
+  const course = await Course.create({
+    title,
+    description,
+    category,
+    createdBy,
+    thumbnail: {
+      public_id: "Dummy",
 
+      secure_url: "Dummy",
+    },
+  });
+  if (!course) {
+    return next(
+      new AppErorr("Course could not be created, please try again ", 500)
+    );
+  }
+
+  if ((req, file)) {
     try {
-        const {id} = req.params;
+      const result = await cloudniry.v2.uploader.upload(req.file.path, {
+        folder: "lms",
+      });
 
-        const course = await Course.findById(id);
-        if(!course){
-        return next(new AppErorr('Invalid course id',400));
-
-        }
-
-        res.status(200).json({
-            success:true,
-            message:"Course lectures fetched successfully!",
-            lectures:course.lectures
-        })
-        
+      if (result) {
+        course.thumbnail.public_id = result.public_id;
+        course.thumbnail.secure_url = result.secure_url;
+      }
+      fs.rm(`uploads/${req.file.filename}`);
     } catch (error) {
-        return next(new AppErorr(error.message,500));
-        
+      return next(new AppErorr(error.message, 500));
     }
-    
-}
 
-export{
-    getAllCourses,
-    getLectureByCourseId,
-}
+    await course.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Course created successfully!",
+      course,
+    });
+  }
+};
+
+const updateCourse = async (req, res, next) => {
+
+  try {
+
+    const {id} = req.params;
+    const course = await Course.findByIdAndUpdate(
+      id,
+      {
+        $set:req.body
+      },
+      {
+        runValidators:true
+      }
+    );
+    
+  if(!course){
+      return next(new AppErorr("Course with given id does not exist", 500));
+
+  }
+
+  res.status(200).json({
+    success:true,
+    message:"Course updated successfully!",
+    course,
+  })
+    
+  } catch (error) {
+      return next(new AppErorr(error.message, 500));
+
+    
+  }
+
+};
+
+const removeCourse = async (req, res, next) => {
+
+  try {
+
+    const {id} = req.params;
+
+    const course = await Course.findById(id);
+
+    if(!course){
+      return next(new AppErorr("Course with given id does not exist", 500));
+
+    }
+
+    await Course.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success:true,
+      message:"Course deleted successfullt!",
+
+    })
+    
+  } catch (error) {
+    return next(new AppErorr(error.message,500));
+  }
+};
+
+export {
+  getAllCourses,
+  getLectureByCourseId,
+  createCourse,
+  updateCourse,
+  removeCourse,
+};
--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,12 +1,26 @@
 import {Router} from 'express';
-import { getAllCourses, getLectureByCourseId } from '../controllers/course.controller.js';
+import { createCourse, getAllCourses, getLectureByCourseId, removeCourse, updateCourse } from '../controllers/course.controller.js';
 import { isLonggedIn } from '../middlewares/auth.middleware.js';
+import upload from '../middlewares/multer.middleware.js';
 
 const router = Router();
 
 router.route('/')
-.get(getAllCourses);
+.get(getAllCourses)
+.post(
+    isLonggedIn,
+    upload.single('thumbnail'),
+    createCourse
+    );
+
+
 router.route('/:id')
-.get(isLonggedIn,getLectureByCourseId);
+.get(isLonggedIn,getLectureByCourseId)
+.put(
+    isLonggedIn,
+    updateCourse)
+.delete(
+    isLonggedIn,
+    removeCourse);
 
 export default router;
