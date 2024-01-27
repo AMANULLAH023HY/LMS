124908ddf815745f9174cc70a020c5e45f7cb1eb
AMANULLAH023HY <amana170829@gmail.com>
2024-01-27 23:25:20 +0530

    Added footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-     <h1 className='text-red-600'>hello</h1> 
+    <Footer/>
     </>
   )
 }
